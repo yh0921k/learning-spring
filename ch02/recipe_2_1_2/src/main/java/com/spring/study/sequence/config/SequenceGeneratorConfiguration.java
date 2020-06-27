cb80a9182f3abb288c37cd02e0b37c8966207329
Yonghwi Kim <yh0921k@gmail.com>
2020-06-27 18:49:22 +0900

    [mod] 기존의 SequenceGenerator 객체를 반환하던 클래스 수정
    - 단순히 @ComponentScan을 통해 IoC 컨테이너의 객체 생성 설정 기능만 처리

--- ch02/recipe_2_1_2/src/main/java/com/spring/study/sequence/config/SequenceGeneratorConfiguration.java
+++ ch02/recipe_2_1_2/src/main/java/com/spring/study/sequence/config/SequenceGeneratorConfiguration.java
@@ -1,23 +1,18 @@
 package com.spring.study.sequence.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import com.spring.study.sequence.SequenceGenerator;
+import org.springframework.context.annotation.FilterType;
 
 // 현재 클래스가 구성 클래스임을 스프링에 알림
 // 아래 어노테이션을 보면 일단 그 안에서 빈 인스턴스 정의부, 즉 @Bean을 붙인(빈 인스턴스를 생성하여 반환하는) 자바 메서드를 찾는다.
 @Configuration
+// 아래의 설정은 Dao 혹은 Service가 포함된 것들은 포함시키고 @Controller를 붙인 클래스는 제외시킨다.
+@ComponentScan(
+    includeFilters = {@ComponentScan.Filter(type = FilterType.REGEX,
+        pattern = {"com.spring.study.sequence.*Dao", "com.spring.study.sequence.*Service"})},
+    excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,
+        classes = {org.springframework.stereotype.Controller.class})})
 public class SequenceGeneratorConfiguration {
 
-  // 아래 어노테이션을 붙이면 해당 메서드와 동일한 이름의 빈이 생성된다.
-  // 빈 이름을 따로 명시하려면 @Bean(name="myBean")과 같이 name 속성을 이용한다.
-  // 빈 이름을 따로 지정하면 메서드명은 무시된다.
-  @Bean
-  public SequenceGenerator sequenceGenerator() {
-    SequenceGenerator seqgen = new SequenceGenerator();
-    seqgen.setPrefix("30");
-    seqgen.setSuffix("A");
-    seqgen.setInitial(100000);
-    return seqgen;
-  }
 }
